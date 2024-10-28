f08d8143cd7931bfbdb5ac290ffab98f3b5349a2
ramasuryananda <gunkrama96@gmail.com>
2024-10-28 13:10:37 +0700

    test(employment): create and delete employment test

--- internal/usecase/employment/employment_test.go
+++ internal/usecase/employment/employment_test.go
@@ -174,3 +174,340 @@ func TestUseCase_GetUserEmployment(t *testing.T) {
 		})
 	}
 }
+
+func TestUseCase_CreateEmploymentData(t *testing.T) {
+	type fields struct {
+		employmentRepository *mockemployment.MockRepositoryProvider
+		profileRepository    *mockprofile.MockRepositoryProvider
+	}
+
+	type args struct {
+		ctx context.Context
+		req request.CreateEmploymentRequest
+	}
+
+	tests := []struct {
+		name           string
+		mock           func(mockfields fields)
+		args           args
+		wantResp       writer.Response
+		wantHttpStatus int
+	}{
+		{
+			name: "success",
+			args: args{
+				ctx: context.Background(),
+				req: request.CreateEmploymentRequest{
+					ProfileCode: userProfile.ProfileCode,
+					JobTitle:    "test job title",
+					Employer:    "test employer",
+					StartDate:   "12-12-2024",
+					EndDate:     "13-12-2024",
+					City:        "test city",
+					Description: "test description",
+				},
+			},
+			mock: func(mocks fields) {
+				mocks.profileRepository.EXPECT().GetUserByProfileCode(context.Background(), userProfile.ProfileCode).Return(userProfile, nil)
+
+				mocks.employmentRepository.EXPECT().CreateEmploymentData(context.Background(), entity.Employment{
+					ProfileCode: userProfile.ProfileCode,
+					JobTitle:    "test job title",
+					Employer:    "test employer",
+					StartDate:   time.Date(2024, 12, 12, 0, 0, 0, 0, time.Now().Location()),
+					EndDate: sql.NullTime{
+						Valid: true,
+						Time:  time.Date(2024, 12, 13, 0, 0, 0, 0, time.Now().Location()),
+					},
+					City:        "test city",
+					Description: "test description",
+				}).Return(uint64(1), nil)
+			},
+			wantResp: writer.Response{
+				Code:    constant.ResponseSuccess.Code,
+				Message: constant.ResponseSuccess.Description,
+				Data: response.CreateEmploymentResponse{
+					Id:          1,
+					ProfileCode: userProfile.ProfileCode,
+				},
+			},
+			wantHttpStatus: constant.ResponseSuccess.Status,
+		},
+		{
+			name: "failed create",
+			args: args{
+				ctx: context.Background(),
+				req: request.CreateEmploymentRequest{
+					ProfileCode: userProfile.ProfileCode,
+					JobTitle:    "test job title",
+					Employer:    "test employer",
+					StartDate:   "12-12-2024",
+					EndDate:     "13-12-2024",
+					City:        "test city",
+					Description: "test description",
+				},
+			},
+			mock: func(mocks fields) {
+				mocks.profileRepository.EXPECT().GetUserByProfileCode(context.Background(), userProfile.ProfileCode).Return(userProfile, nil)
+
+				mocks.employmentRepository.EXPECT().CreateEmploymentData(context.Background(), entity.Employment{
+					ProfileCode: userProfile.ProfileCode,
+					JobTitle:    "test job title",
+					Employer:    "test employer",
+					StartDate:   time.Date(2024, 12, 12, 0, 0, 0, 0, time.Now().Location()),
+					EndDate: sql.NullTime{
+						Valid: true,
+						Time:  time.Date(2024, 12, 13, 0, 0, 0, 0, time.Now().Location()),
+					},
+					City:        "test city",
+					Description: "test description",
+				}).Return(uint64(0), assert.AnError)
+			},
+			wantResp: writer.Response{
+				Code:    constant.ResponseInternalServerError.Code,
+				Message: constant.ResponseInternalServerError.Description,
+			},
+			wantHttpStatus: constant.ResponseInternalServerError.Status,
+		},
+		{
+			name: "error parsing date",
+			args: args{
+				ctx: context.Background(),
+				req: request.CreateEmploymentRequest{
+					ProfileCode: userProfile.ProfileCode,
+					JobTitle:    "test job title",
+					Employer:    "test employer",
+					StartDate:   "12-12-2024",
+					EndDate:     "13-12-20245",
+					City:        "test city",
+					Description: "test description",
+				},
+			},
+			mock: func(mocks fields) {
+				mocks.profileRepository.EXPECT().GetUserByProfileCode(context.Background(), userProfile.ProfileCode).Return(userProfile, nil)
+			},
+			wantResp: writer.Response{
+				Code:    constant.ResponseBadRequest.Code,
+				Message: constant.ResponseBadRequest.Description,
+			},
+			wantHttpStatus: constant.ResponseBadRequest.Status,
+		},
+		{
+			name: "error parsing date",
+			args: args{
+				ctx: context.Background(),
+				req: request.CreateEmploymentRequest{
+					ProfileCode: userProfile.ProfileCode,
+					JobTitle:    "test job title",
+					Employer:    "test employer",
+					StartDate:   "12-12-20245",
+					EndDate:     "13-12-2024",
+					City:        "test city",
+					Description: "test description",
+				},
+			},
+			mock: func(mocks fields) {
+				mocks.profileRepository.EXPECT().GetUserByProfileCode(context.Background(), userProfile.ProfileCode).Return(userProfile, nil)
+			},
+			wantResp: writer.Response{
+				Code:    constant.ResponseBadRequest.Code,
+				Message: constant.ResponseBadRequest.Description,
+			},
+			wantHttpStatus: constant.ResponseBadRequest.Status,
+		},
+		{
+			name: "failed get data",
+			args: args{
+				ctx: context.Background(),
+				req: request.CreateEmploymentRequest{
+					ProfileCode: userProfile.ProfileCode,
+					JobTitle:    "test job title",
+					Employer:    "test employer",
+					StartDate:   "12-12-20245",
+					EndDate:     "13-12-2024",
+					City:        "test city",
+					Description: "test description",
+				},
+			},
+			mock: func(mocks fields) {
+				mocks.profileRepository.EXPECT().GetUserByProfileCode(context.Background(), userProfile.ProfileCode).Return(entity.Profile{}, constant.ErrorDatabaseNotFound)
+			},
+			wantResp: writer.Response{
+				Code:    constant.ResponseErrorNotFound.Code,
+				Message: constant.ResponseErrorNotFound.Description,
+			},
+			wantHttpStatus: constant.ResponseErrorNotFound.Status,
+		},
+		{
+			name: "failed get data profile",
+			args: args{
+				ctx: context.Background(),
+				req: request.CreateEmploymentRequest{
+					ProfileCode: userProfile.ProfileCode,
+					JobTitle:    "test job title",
+					Employer:    "test employer",
+					StartDate:   "12-12-20245",
+					EndDate:     "13-12-2024",
+					City:        "test city",
+					Description: "test description",
+				},
+			},
+			mock: func(mocks fields) {
+				mocks.profileRepository.EXPECT().GetUserByProfileCode(context.Background(), userProfile.ProfileCode).Return(entity.Profile{}, assert.AnError)
+			},
+			wantResp: writer.Response{
+				Code:    constant.ResponseInternalServerError.Code,
+				Message: constant.ResponseInternalServerError.Description,
+			},
+			wantHttpStatus: constant.ResponseInternalServerError.Status,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			fields := fields{
+				profileRepository:    mockprofile.NewMockRepositoryProvider(ctrl),
+				employmentRepository: mockemployment.NewMockRepositoryProvider(ctrl),
+			}
+
+			tt.mock(fields)
+
+			uc := &UseCase{
+				profileRepository:    fields.profileRepository,
+				employmentRepository: fields.employmentRepository,
+			}
+			gotResp, gotHttpStatus := uc.CreateEmploymentData(tt.args.ctx, tt.args.req)
+			if !reflect.DeepEqual(gotResp, tt.wantResp) {
+				t.Errorf("UseCase.CreateEmploymentData() gotResp = %v, want %v", gotResp, tt.wantResp)
+			}
+			if gotHttpStatus != tt.wantHttpStatus {
+				t.Errorf("UseCase.CreateEmploymentData() gotHttpStatus = %v, want %v", gotHttpStatus, tt.wantHttpStatus)
+			}
+		})
+	}
+}
+
+func TestUseCase_DeleteEmploymentData(t *testing.T) {
+	type fields struct {
+		employmentRepository *mockemployment.MockRepositoryProvider
+		profileRepository    *mockprofile.MockRepositoryProvider
+	}
+	type args struct {
+		ctx context.Context
+		req request.DeleteEmploymentRequest
+	}
+	tests := []struct {
+		name           string
+		mock           func(mocks fields)
+		args           args
+		wantResp       writer.Response
+		wantHttpStatus int
+	}{
+		{
+			name: "success",
+			args: args{
+				ctx: context.Background(),
+				req: request.DeleteEmploymentRequest{
+					ProfileCode: userProfile.ProfileCode,
+					ID:          1,
+				},
+			},
+			mock: func(mocks fields) {
+				mocks.employmentRepository.EXPECT().GetFirstEmploymentByProfileCodeandID(context.Background(), userProfile.ProfileCode, uint64(1)).Return(employmentData, nil)
+
+				mocks.employmentRepository.EXPECT().DeleteEmploymentData(context.Background(), employmentData.ProfileCode, employmentData.ID).Return(nil)
+			},
+			wantResp: writer.Response{
+				Code:    constant.ResponseSuccess.Code,
+				Message: constant.ResponseSuccess.Description,
+				Data: response.DeleteEmploymentResponse{
+					ProfileCode: userProfile.ProfileCode,
+				},
+			},
+			wantHttpStatus: constant.ResponseSuccess.Status,
+		},
+		{
+			name: "failed delete",
+			args: args{
+				ctx: context.Background(),
+				req: request.DeleteEmploymentRequest{
+					ProfileCode: userProfile.ProfileCode,
+					ID:          1,
+				},
+			},
+			mock: func(mocks fields) {
+				mocks.employmentRepository.EXPECT().GetFirstEmploymentByProfileCodeandID(context.Background(), userProfile.ProfileCode, uint64(1)).Return(employmentData, nil)
+
+				mocks.employmentRepository.EXPECT().DeleteEmploymentData(context.Background(), employmentData.ProfileCode, employmentData.ID).Return(assert.AnError)
+			},
+			wantResp: writer.Response{
+				Code:    constant.ResponseInternalServerError.Code,
+				Message: constant.ResponseInternalServerError.Description,
+			},
+			wantHttpStatus: constant.ResponseInternalServerError.Status,
+		},
+		{
+			name: "failed delete not found",
+			args: args{
+				ctx: context.Background(),
+				req: request.DeleteEmploymentRequest{
+					ProfileCode: userProfile.ProfileCode,
+					ID:          1,
+				},
+			},
+			mock: func(mocks fields) {
+				mocks.employmentRepository.EXPECT().GetFirstEmploymentByProfileCodeandID(context.Background(), userProfile.ProfileCode, uint64(1)).Return(entity.Employment{}, constant.ErrorDatabaseNotFound)
+			},
+			wantResp: writer.Response{
+				Code:    constant.ResponseErrorNotFound.Code,
+				Message: constant.ResponseErrorNotFound.Description,
+			},
+			wantHttpStatus: constant.ResponseErrorNotFound.Status,
+		},
+		{
+			name: "failed delete get data",
+			args: args{
+				ctx: context.Background(),
+				req: request.DeleteEmploymentRequest{
+					ProfileCode: userProfile.ProfileCode,
+					ID:          1,
+				},
+			},
+			mock: func(mocks fields) {
+				mocks.employmentRepository.EXPECT().GetFirstEmploymentByProfileCodeandID(context.Background(), userProfile.ProfileCode, uint64(1)).Return(entity.Employment{}, assert.AnError)
+			},
+			wantResp: writer.Response{
+				Code:    constant.ResponseInternalServerError.Code,
+				Message: constant.ResponseInternalServerError.Description,
+			},
+			wantHttpStatus: constant.ResponseInternalServerError.Status,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			fields := fields{
+				profileRepository:    mockprofile.NewMockRepositoryProvider(ctrl),
+				employmentRepository: mockemployment.NewMockRepositoryProvider(ctrl),
+			}
+
+			tt.mock(fields)
+
+			uc := &UseCase{
+				profileRepository:    fields.profileRepository,
+				employmentRepository: fields.employmentRepository,
+			}
+			gotResp, gotHttpStatus := uc.DeleteEmploymentData(tt.args.ctx, tt.args.req)
+			if !reflect.DeepEqual(gotResp, tt.wantResp) {
+				t.Errorf("UseCase.DeleteEmploymentData() gotResp = %v, want %v", gotResp, tt.wantResp)
+			}
+			if gotHttpStatus != tt.wantHttpStatus {
+				t.Errorf("UseCase.DeleteEmploymentData() gotHttpStatus = %v, want %v", gotHttpStatus, tt.wantHttpStatus)
+			}
+		})
+	}
+}
